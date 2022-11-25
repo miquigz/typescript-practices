0ea80bdc1db13daee9c3300fa6aab7bacea993f8
Miqueas Gimenez <miquigz02@gmail.com>
2022-11-25 16:10:40 -0300

    exercises 13,14

---
+++ LongestCommonPrefix.ts
@@ -0,0 +1,23 @@
+function longestCommonPrefix(strs: string[]): string {
+    let prefix = "";
+    let continuar:boolean = true;
+    let actual:number = 0;
+    while (continuar){
+        prefix += strs[0].split('')[actual]
+        continuar = (strs.filter( (ele)=> ele.split('')[actual] === prefix[actual])).length === strs.length;
+        if (!continuar){
+            prefix = prefix.slice(0,-1);
+        }
+        actual++;
+    }
+    if (strs.length === 1 && strs[0] === '')
+        prefix = '';
+    return prefix || '';
+};
+
+console.log(longestCommonPrefix(["flower","flow","flight"]));
+console.log(longestCommonPrefix(['']));
+console.log(longestCommonPrefix(['asdw', 'wdasd','fkssa']))
+
+//Write a function to find the longest common prefix string amongst an array of strings.
+//If there is no common prefix, return an empty string "".
